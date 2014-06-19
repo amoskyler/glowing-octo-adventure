6b8f5b017b6b178e82afe942590f3e280bc47a73
amoskyler <amos.kyler@gmail.com>
2014-06-18 18:12:39 -0700

    basic models/routes/views setup for backend SPA

--- public/js/main.js
+++ public/js/main.js
@@ -1,27 +1,41 @@
-var IndexView = Backbone.View.extend({
-    render:function(){
-      return this;
+$(document).ready(function(){
+
+  var IndexModel = Backbone.Model.extend({
+    defaults:{
+      title: 'test'
     }
-});
+  });
 
-var AppRouter = Backbone.Router.extend({
-  routes: {
-    '': 'index', //#inex
-    'item/:id': 'item' //#item
+  var newStuff = new IndexModel();
+
+
+var ItemView = Backbone.View.extend({
+  el: '.content',
+  initialize: function(){
+    this.render();
   },
 
-  index: function(){
-    console.log("you're at the index");
-    view = new IndexView();
-  },
-
-  item: function(id){
-    console.log(id);
+  render:function(){
+    this.$el.html(this.model.get('title'));
+    return this;
   }
 });
 
-var appModel = Backbone.Model.extend({
 
+var IndexView = Backbone.View.extend({
+    render:function(){
+      this.$el.html("test of route <button>test</button>");
+      return this;
+    },
+    events: {
+      "click button": "doStuff"
+    },
+    doStuff: function() {
+      this.$el.html("gone");
+      //var view = new ItemView({model: newStuff});
+      //$('.content').html(view.render().el);
+      Backbone.history.navigate('/item/555',{trigger:false});
+    }
 });
 
 
@@ -31,9 +45,40 @@ var appModel = Backbone.Model.extend({
 
 
 
+  var AppRouter = Backbone.Router.extend({
+    routes: {
+      '': 'index', //#index
+      'item/:id': 'item' //#item
+    },
+
+    index: function(){
+      console.log("you're at the index");
+      var view = new IndexView();
+      $('.content').html(view.render().el);
+    },
+
+    item: function(id){
+      console.log(id);
+      var view = new ItemView({model: newStuff});
+    }
+  });
+
+  var appModel = Backbone.Model.extend({
+
+  });
 
 
 
 
-var appRouter = new AppRouter();
-Backbone.history.start({pushState: true});
+
+
+
+
+
+
+
+
+  var appRouter = new AppRouter();
+  Backbone.history.start({pushState: true});
+
+});
